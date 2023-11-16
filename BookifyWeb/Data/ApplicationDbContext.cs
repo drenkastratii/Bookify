fc23ebf06be0f82a3ee17e0ffd7ea628a8f44bc4
Era Qerimi <eq58315@ubt-uni.net>
2023-11-16 17:53:56 +0100

    feat; Seeded the category table

--- BookifyWeb/Data/ApplicationDbContext.cs
+++ BookifyWeb/Data/ApplicationDbContext.cs
@@ -12,5 +12,17 @@ namespace BookifyWeb.Data
         }
 
         DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id=1, Name="Action"},
+                new Category { Id=2, Name="Drama"},
+                new Category { Id=3, Name="SciFI"}
+
+                );
+        }
     }
 }
