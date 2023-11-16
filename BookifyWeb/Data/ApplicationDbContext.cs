c0f0c3e405f89f5ed7b714e5c590d958f186cd00
Era Qerimi <eq58315@ubt-uni.net>
2023-11-16 20:05:16 +0100

    feat; Implementation of application DB context and retrieving all the categories

--- BookifyWeb/Data/ApplicationDbContext.cs
+++ BookifyWeb/Data/ApplicationDbContext.cs
@@ -11,7 +11,7 @@ namespace BookifyWeb.Data
         {
         }
 
-        DbSet<Category> Categories { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
