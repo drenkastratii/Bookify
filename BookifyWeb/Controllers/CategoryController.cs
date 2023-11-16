3fb3e3d70a6d513e22c9cb2d27bc38f82402466d
Era Qerimi <eq58315@ubt-uni.net>
2023-11-16 18:42:11 +0100

    feat; Added the Category Controller

---
+++ BookifyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookifyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
