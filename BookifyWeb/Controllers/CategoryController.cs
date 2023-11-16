3733758b9a65b71be5161ea545a39a82339a88a7
Era Qerimi <eq58315@ubt-uni.net>
2023-11-16 20:22:52 +0100

    feat; Added List items in Category table

--- BookifyWeb/Controllers/CategoryController.cs
+++ BookifyWeb/Controllers/CategoryController.cs
@@ -14,7 +14,7 @@ namespace BookifyWeb.Controllers
         public IActionResult Index()
         {
             List<Category> objCategoryList = _db.Categories.ToList();
-            return View();
+            return View(objCategoryList);
         }
     }
 }
