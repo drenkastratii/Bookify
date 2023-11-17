1d329d28750fb4dae04678de7077e59f880f5771
Era Qerimi <eq58315@ubt-uni.net>
2023-11-17 11:55:00 +0100

    feat; Added the Edit Action in the controller Category

--- BookifyWeb/Controllers/CategoryController.cs
+++ BookifyWeb/Controllers/CategoryController.cs
@@ -20,5 +20,47 @@ namespace BookifyWeb.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();   
+            
+            
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id==null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+
+        }
     }
 }
