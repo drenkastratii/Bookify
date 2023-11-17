49940d5fb299dd4524a602f5f54ded0fdb1dc651
Era Qerimi <eq58315@ubt-uni.net>
2023-11-17 12:27:11 +0100

    feat; Added Partial Action to Crud proces

--- BookifyWeb/Controllers/CategoryController.cs
+++ BookifyWeb/Controllers/CategoryController.cs
@@ -28,6 +28,7 @@ namespace BookifyWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             return View();   
@@ -56,6 +57,7 @@ namespace BookifyWeb.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
             return View();
@@ -86,6 +88,7 @@ namespace BookifyWeb.Controllers
             }
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
         }
     }
