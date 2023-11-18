422ac816ee3ce8e7679b156885b0e599cd2a0528
BlendSejdiu <119117327+BlendSejdiu@users.noreply.github.com>
2023-11-18 13:35:34 +0100

    feat; Added author controller

---
+++ BookifyWeb/Controllers/AuthorController.cs
@@ -0,0 +1,28 @@
+﻿using BookifyWeb.Data;
+using BookifyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookifyWeb.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public AuthorController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Author> objAuthList = _db.Authors.ToList();
+            return View(objAuthList);
+        }
+
+        public IActionResult Create() 
+        { 
+            return View();
+        }
+        
+
+
+    }
+}
