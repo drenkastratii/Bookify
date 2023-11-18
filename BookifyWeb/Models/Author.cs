cd388b96649371720b88370d7e00d364fac8aa25
BlendSejdiu <119117327+BlendSejdiu@users.noreply.github.com>
2023-11-18 13:34:02 +0100

    feat; Added author model

---
+++ BookifyWeb/Models/Author.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookifyWeb.Models
+{
+    public class Author
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? FullName { get; set; }
+
+    }
+}
