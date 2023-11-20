04ab258353526bbfeeeed1194ada038199b235a9
Dren Kastrati <drenkastrati33@gmail.com>
2023-11-20 11:06:18 +0100

    feat: Added the Book Repositiory class

---
+++ Bookify.Data/Repository/BookRepository.cs
@@ -0,0 +1,23 @@
+﻿using Bookify.Data.Repository.IRepository;
+using Bookify.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Data.Repository
+{
+    internal class BookRepository : Repository<Book>, IBookRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public BookRepository(ApplicationDbContext db) :base(db)
+        {
+            _db = db;
+        }
+        public void Update(Book obj)
+        {
+            _db.Update(obj);
+        }
+    }
+}
