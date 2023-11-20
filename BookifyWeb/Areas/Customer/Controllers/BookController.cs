85028924254f866a57afe745642d7d5ed0e13bc4
Dren Kastrati <drenkastrati33@gmail.com>
2023-11-20 11:34:42 +0100

    feat; Adding the Create Action Method to the Book Controller

---
+++ BookifyWeb/Areas/Customer/Controllers/BookController.cs
@@ -0,0 +1,49 @@
+﻿using Bookify.Data.Repository.IRepository;
+using Bookify.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookifyWeb.Areas.Admin.Controllers
+{
+    [Area("Customer")]
+
+    public class BookController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public BookController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Book> objBookList = _unitOfWork.Book.GetAll().ToList();
+            return View(objBookList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Book obj)
+        {
+            var existingBook = _unitOfWork.Book.Get(c => c.Title.ToLower() == obj.Title.ToLower());
+            if (existingBook != null)
+            {
+                ModelState.AddModelError("Title", "The Book Already Exists");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Book.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Book created successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+
+
+        }
+
+        
+    }
+}
+
