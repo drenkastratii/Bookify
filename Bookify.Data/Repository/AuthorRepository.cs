303e3658b3d600866e6270b366564fdd8ed7219d
Dren Kastrati <drenkastrati33@gmail.com>
2023-11-20 09:00:56 +0100

    feat; Added the AuthorRepository Class

---
+++ Bookify.Data/Repository/AuthorRepository.cs
@@ -0,0 +1,23 @@
+﻿using Bookify.Data.Repository.IRepository;
+using Bookify.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Data.Repository
+{
+    public class AuthorRepository : Repository<Author>, IAuthorRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public AuthorRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(Author obj)
+        {
+            _db.Update(obj);
+        }
+    }
+}
