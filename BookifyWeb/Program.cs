1256e879343578131fad50e934a6e4caef25178a
Dren Kastrati <drenkastrati33@gmail.com>
2023-11-20 09:30:12 +0100

    feat; Added the UnitOfWork Service in our program file

--- BookifyWeb/Program.cs
+++ BookifyWeb/Program.cs
@@ -1,4 +1,6 @@
 using Bookify.Data;
+using Bookify.Data.Repository;
+using Bookify.Data.Repository.IRepository;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,6 +15,9 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+//Repositories
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
