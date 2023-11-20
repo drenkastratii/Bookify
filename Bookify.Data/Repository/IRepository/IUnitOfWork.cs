ee48d725e11a4071e1372f9f56b2a3cf26e223aa
Dren Kastrati <drenkastrati33@gmail.com>
2023-11-20 09:03:37 +0100

    feat; Added the IUnitOfWork Interface

---
+++ Bookify.Data/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Data.Repository.IRepository
+{
+    public interface IUnitOfWork 
+    {
+        public ICategoryRepository Category { get; }
+        public IAuthorRepository Author { get; }
+
+        void Save();
+    }
+}
