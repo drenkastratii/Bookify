319bea37b97f055b66f6efa46e1cc0dc8124b280
Dren Kastrati <drenkastrati33@gmail.com>
2023-11-20 10:40:34 +0100

    feat; Initial Book Model

---
+++ Bookify.Model/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Models.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(1,100)]
+        public double Price { get; set; }
+    }
+}
