fc5a5b5526f05ce3c551bfda51fe3170132a2664
Dren Kastrati <drenkastrati33@gmail.com>
2023-11-20 11:37:09 +0100

    feat; Added the Edit Action Method to the Book Controller

--- BookifyWeb/Areas/Customer/Controllers/BookController.cs
+++ BookifyWeb/Areas/Customer/Controllers/BookController.cs
@@ -43,6 +43,39 @@ namespace BookifyWeb.Areas.Admin.Controllers
 
         }
 
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Book? bookFromDb = _unitOfWork.Book.Get(c => c.Id == id);
+            if (bookFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(bookFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Book obj)
+        {
+            var existingBook = _unitOfWork.Book.Get(c => c.Title.ToLower() == obj.Title.ToLower());
+            if (existingBook != null)
+            {
+                ModelState.AddModelError("Title", "The Book Already Exists");
+            }
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Book.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Book updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+
+
+        }
+
         
     }
 }
