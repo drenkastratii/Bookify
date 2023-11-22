299080017d738dd1a22fcc54a0b8d9a3824ff411
Dren Kastrati <drenkastrati33@gmail.com>
2023-11-22 13:36:30 +0100

    feat; loading the datatable

---
+++ BookifyWeb/wwwroot/js/book.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    datatable = $('#tblData').DataTable({
+        "ajax": {url: '/admin/book/getall'},
+        "columns": [
+            { data: 'title', "width" : "25%" },
+            { data: 'description', "width": "15%" },
+            { data: 'price', "width": "5%" },
+            { data: 'category.name', "width": "15%" },
+            { data: 'author.fullName', "width": "15%" }
+        ]
+    });
+}
+
+
+
