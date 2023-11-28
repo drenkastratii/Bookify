c44adabff1cc1d3025c64d89353555452c74667c
Dren Kastrati <drenkastrati33@gmail.com>
2023-11-28 18:05:54 +0100

    feat; displaying content based on role

--- BookifyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BookifyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 using Bookify.Data.Repository.IRepository;
 using Bookify.Models;
+using Bookify.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -10,16 +13,27 @@ namespace BookifyWeb.Areas.Customer.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IUnitOfWork _unitOfWork;
+        //private readonly UserManager<IdentityUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork/*, UserManager<IdentityUser> userManager*/)
         {
             _logger = logger;
             _unitOfWork = unitOfWork;
+            //_userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             IEnumerable<Book> bookList = _unitOfWork.Book.GetAll(includeProperties: "Category,Author");
+            //var user = await _userManager.GetUserAsync(User) as ApplicationUser;
+            //if (user != null)
+            //{
+            //    var role = await _userManager.GetRolesAsync(user);
+            //    if (role.FirstOrDefault() == SD.Role_Admin)
+            //    {
+            //        return RedirectToPage("/Account/Manage/Index", new { area = "Identity" });
+            //    }
+            //}
             return View(bookList);
         }
 
