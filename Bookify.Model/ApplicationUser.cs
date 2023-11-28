e7ce3ffb84729b3c4ddffc82d2cb91a95dd80b4d
Dren Kastrati <drenkastrati33@gmail.com>
2023-11-28 18:36:12 +0100

    feat; adding other input fields for registration purposes

--- Bookify.Model/ApplicationUser.cs
+++ Bookify.Model/ApplicationUser.cs
@@ -12,7 +12,6 @@ namespace Bookify.Models
     {
         [Required]
         public string Name {  get; set; }   
-
         public string? StreetAddress { get; set; }
         public string? City { get; set; }
         public string? State { get; set; }
