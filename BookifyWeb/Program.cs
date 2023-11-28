5f2b49c5fa85ea703fe0fe3dd3a9fd7e3fbaee0c
Dren Kastrati <drenkastrati33@gmail.com>
2023-11-28 09:58:58 +0100

    feat; making the identity account folder visible for future override

--- BookifyWeb/Program.cs
+++ BookifyWeb/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(/*options => options.SignIn.RequireConfirmedAccount = true*/)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
@@ -37,7 +37,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication(); //e vendosim para autorizimit
 app.UseAuthorization();
 
 app.MapControllerRoute(
