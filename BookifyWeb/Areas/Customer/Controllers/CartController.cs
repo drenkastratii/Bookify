25d60f83b853219000145819922fa3b4def9741b
Dren Kastrati <drenkastrati33@gmail.com>
2023-12-02 15:21:07 +0100

    fix; tracked parameter set to true for removing ShoppingCarts(we made defualt false)

--- BookifyWeb/Areas/Customer/Controllers/CartController.cs
+++ BookifyWeb/Areas/Customer/Controllers/CartController.cs
@@ -181,7 +181,7 @@ namespace BookifyWeb.Areas.Customer.Controllers
 
         public IActionResult Minus(int cartId)
         {
-            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId, tracked: true);
             if (cartFromDb.Count <= 1)
             {
                 // e largojme prej cart
@@ -201,7 +201,7 @@ namespace BookifyWeb.Areas.Customer.Controllers
 
         public IActionResult Remove(int cartId)
         {
-            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId,tracked:true);
             HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart
                     .GetAll(u => u.ApplicationUserId == cartFromDb.ApplicationUserId).Count() - 1);
             _unitOfWork.ShoppingCart.Remove(cartFromDb);
