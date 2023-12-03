7c5826d8d8647b59dcadfdd25b54255468d1c023
Dren Kastrati <drenkastrati33@gmail.com>
2023-12-03 10:56:37 +0100

    feat; adding the lock/unlock button and their functionality

--- BookifyWeb/wwwroot/js/user.js
+++ BookifyWeb/wwwroot/js/user.js
@@ -11,16 +11,50 @@ function loadDataTable() {
             { "data": "name", "width": "15%" },
             { "data": "email", "width": "15%" },
             { "data": "phoneNumber", "width": "15%" },
+            { "data": "state", "width": "15%" },
             { "data": "role", "width": "15%" },
             {
-                data: 'id',
+                data: { id: "id", lockoutEnd: "lockoutEnd" },
                 "render": function (data) {
-                    return `<div class="w-75 btn-group" role="group">
-                     <a href="/admin/user/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
-                    </div>`
+                    var today = new Date().getTime();
+                    var lockout = new Date(data.lockoutEnd).getTime();
+
+                    if (lockout > today) {
+                        return `
+                        <div class="text-center">
+                            <a onclick=LockUnlock('${data.id}') class="btn btn-success text-white" style="cursor:pointer; width:100px;">
+                            <i class="bi bi-unlock-fill"></i>  UnLock
+                                </a>
+                        </div>
+                    `
+                    }
+                    else {
+                        return `
+                        <div class="text-center">
+                             <a onclick=LockUnlock('${data.id}') class="btn btn-danger text-white" style="cursor:pointer; width:100px;">
+                                <i class="bi bi-lock-fill"></i>  Lock
+                             </a> 
+                        </div>
+                    `
+                    }
                 },
-                "width": "25%"
+                "width": "15%"
             }
         ]
+    });
+}
+
+function LockUnlock(id) {
+    $.ajax({
+        type: "POST",
+        url: '/Admin/User/LockUnlock',
+        data: JSON.stringify(id),
+        contentType: "application/json",
+        success: function (data) {
+            if (data.success) {
+                toastr.success(data.message);
+                dataTable.ajax.reload();
+            }
+        }
     });
 }
