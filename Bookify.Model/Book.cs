863cdd768877bfe3d9d79246e6295353bd51303f
Dren Kastrati <drenkastrati33@gmail.com>
2023-12-03 19:21:15 +0100

    fix; the display field author and category look up

--- Bookify.Model/Book.cs
+++ Bookify.Model/Book.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -24,12 +25,14 @@ namespace Bookify.Models
         [Required]
         [Range(1, 100)]
         public double Price { get; set; }
-
+        [DisplayName("Category")]
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
         [ValidateNever]
         public Category Category { get; set; }
 
+
+        [DisplayName("Author")]
         public int AuthorId { get; set; }
         [ForeignKey("AuthorId")]
         [ValidateNever]
